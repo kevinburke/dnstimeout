3fc912570b64e2432193be04cf6158cd93b48b24
Kevin Burke <kev@inburke.com>
2015-01-18 21:46:07 -0800

    add working function

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,51 @@
 use std::io;
 use std::io::net::addrinfo;
 use std::io::net::ip;
+use std::io::timer;
+use std::sync::mpsc;
+use std::sync::mpsc::channel;
+use std::thread;
 use std::time;
 
-pub fn lookup(host: &str, timeout_duration: time::Duration) -> io::IoResult<Vec<ip::IpAddr>> {
-    return addrinfo::get_host_addresses(host)
+pub fn lookup(host: String, timeout_duration: time::Duration) -> io::IoResult<Vec<ip::IpAddr>> {
+    let (tx, rx):(mpsc::Sender<io::IoResult<Vec<ip::IpAddr>>>, mpsc::Receiver<io::IoResult<Vec<ip::IpAddr>>>) = channel();
+    let mut timer = timer::Timer::new().unwrap();
+    let timeout = timer.oneshot(timeout_duration);
+
+    let detail = format!("Failed to resolve {} after {} milliseconds", 
+                         host, timeout_duration.num_milliseconds());
+    thread::Thread::spawn(move|| {
+        // Reading the recverror docs, I'm not sure how this can fail.
+        tx.send(addrinfo::get_host_addresses(host.as_slice()));
+    });
+
+    loop {
+        select! {
+            val = rx.recv() => { return val.unwrap() },
+            _ = timeout.recv() => {
+                let e = io::IoError{
+                    kind: io::IoErrorKind::TimedOut,
+                    desc: "DNS lookup timed out",
+                    detail: Some(detail)
+                };
+                return Err(e)
+            }
+        }
+    }
 }
 
 #[test]
 fn test_lookup() {
-    let res = lookup("api.twilio.com", time::Duration::seconds(3));
+    let res = lookup("api.twilio.com".to_string(), time::Duration::seconds(7));
     match res {
         Ok(addrs) => {
+            if addrs.len() == 0 {
+                panic!("expected to get addresses back, got an empty list.")
+            }
             for addr in addrs.iter() {
                 println!("{}", addr);
             }
-            panic!("foobar")
         }
-        Err(e) => { panic!(e) }
+        Err(e) => { println!("{}", e); panic!(e) }
     }
 }
